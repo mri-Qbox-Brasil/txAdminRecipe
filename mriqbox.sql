583f72db89ff05689b7d12e83ad0772960821026
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-11 01:01:57 -0300

    update sql

--- mriqbox.sql
+++ mriqbox.sql
@@ -210,37 +210,6 @@ CREATE TABLE IF NOT EXISTS `player_vehicles` (
   KEY `license` (`license`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-DELIMITER //
-CREATE TRIGGER rhd_garage_update_impound_plate
-AFTER UPDATE ON player_vehicles
-FOR EACH ROW
-BEGIN
-    UPDATE police_impound
-    SET plate = NEW.plate
-    WHERE plate = OLD.plate;
-END;
-
-CREATE TRIGGER rhd_garage_delete_from_impound
-AFTER DELETE ON player_vehicles
-FOR EACH ROW
-BEGIN
-    DELETE FROM police_impound
-    WHERE plate = OLD.plate;
-END;
-
-CREATE TRIGGER rhd_garage_state_update
-AFTER UPDATE ON player_vehicles
-FOR EACH ROW
-BEGIN
-    IF NEW.state <> 2 THEN
-        DELETE FROM police_impound
-        WHERE plate = OLD.plate;
-    END IF;
-END;
-//
-DELIMITER ;
-
 CREATE TABLE IF NOT EXISTS `player_warns` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `senderIdentifier` varchar(50) DEFAULT NULL,
